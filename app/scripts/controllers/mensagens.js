063500af8fed2c018891ba843ba528c7236934da
Baracasfig <baracasfig@gmail.com>
2014-11-18 14:05:39 -0200

    changes in bower

--- app/scripts/controllers/mensagens.js
+++ app/scripts/controllers/mensagens.js
@@ -1,12 +1,27 @@
 'use strict';
 
 angular.module('inclusaoDigitalApp')
-  .controller('MensagensCtrl', function ($scope, localStorageService) {
+  .controller('MensagensCtrl', function ($scope, localStorageService, $firebase) {
     $scope.messageType = 'recebidas';
     $scope.sendEmail = false;
     $scope.messageDetail = false;
     $scope.message = {};
-    $scope.userEmail = localStorageService.get('email');
+    $scope.ref = new Firebase('https://blistering-torch-9877.firebaseio.com');
+    $scope.sync = $firebase($scope.ref);
+    $scope.messagesRef = $scope.ref.child('messages');
+
+    $scope.list = $scope.sync.$asArray();
+    $scope.list.$loaded().then(function() {
+         console.log("list has " + list.length + " items");
+      });
+    $scope.limited = $firebase($scope.messagesRef.limitToLast(10))
+
+    $scope.limited.on("value", function(snapshot) {
+      $scope.messages = snapshot.val();
+    });
+
+    // identify the author
+    $scope.message.author = localStorageService.get('email');
 
     $scope.isSentMessage = function() {
         $scope.messageType = 'enviadas';
@@ -27,12 +42,27 @@ angular.module('inclusaoDigitalApp')
         $scope.messageDetail = true;
       };
 
+    $scope.sendMessage = function() {
+      $scope.messagesRef.push({
+        author: $scope.message.author,
+        receiver: $scope.message.receiver,
+        subject: $scope.message.subject,
+        contentOfMessage: $scope.message.contentOfMessage
+      });
+      // clean
+      $scope.message.receiver = null;
+      $scope.message.subject = null;
+      $scope.message.contentOfMessage = null;
+    };
+      
+
+
 
     // message example
 
 
-    $scope.message.subject = 'Primeiro e-mail';
-    $scope.message.author = 'Prof. Mariani';
-    $scope.message.day = '10/10/2014';
-    $scope.message.contentOfMessage = 'Olá, \n caros alunos do ID, \n as aulas começam amanhã!';
+    // $scope.message.subject = 'Primeiro e-mail';
+    // $scope.message.author = 'Prof. Mariani';
+    // $scope.message.day = '10/10/2014';
+    // $scope.message.contentOfMessage = 'Olá, \n caros alunos do ID, \n as aulas começam amanhã!';
   });
